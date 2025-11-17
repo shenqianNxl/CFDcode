d725a37818bd68c3216aeebe30774c835298b616
jyyj <1758202026@qq.com>
2025-11-17 15:01:38 +0800

    Fixing some bugs,working on building BCs

---
+++ src/TreatBCs_Euler2d.cpp
@@ -0,0 +1,7 @@
+#include "TreatBCs_Euler2d.h"
+
+
+void TreatBCs_Euler2d(std::vector<std::vector<std::vector<double>>>& U_p){
+
+    
+}
---
+++ src/TreatBCs_Euler2d.h
@@ -0,0 +1,12 @@
+#ifndef TREATBCS_EULER2D_H
+#define TREATBCS_EULER2D_H
+
+#include <vector>
+void TreatBCs_Euler2d(std::vector<std::vector<std::vector<double>>>& U_p);
+
+
+#endif // TREATBCS_EULER2D_H
+
+
+
+
